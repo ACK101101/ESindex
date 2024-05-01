bb4cac688efed1614ab131e757680cb2b51c7699
alex kumar <alexander.kumar@duke.edu>
2024-05-01 14:13:45 -0400

    dataclass

---
+++ BaselineLinear.py
@@ -0,0 +1,15 @@
+## Baseline Linear
+'''
+Return the
+
+'''
+
+# hash function to take in string
+    # output k length digest
+# linear model take in k length digest
+    # output predicted index
+
+class BaselineLinear(data: DataArray):
+
+    def sort_by_digest():
+        pass
