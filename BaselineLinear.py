5165a69fefe254c51a869efd49518110d047e0a3
Marcus <marcusjamesortiz@gmail.com>
2024-05-01 14:14:17 -0400

    outline for baseline

---
+++ BaselineLinear.py
@@ -0,0 +1,23 @@
+## Baseline Linear
+'''
+Baseline Linear Model
+- Take in data
+- Hash data
+- Sort data by hash
+- Linear model to predict index
+'''
+# hash function to take in string
+    # output k length digest
+# linear model take in k length digest
+    # output predicted index
+
+class BaselineLinear:
+    def __init__(data: DataFrame):
+        self.data = data
+
+    def hash(data: DataFrame):
+
+        pass
+    
+    def sort_by_digest():
+        pass
