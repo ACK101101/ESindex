d296b05d7d4766d6aa6af79de3feccb83f65f4cb
alex kumar <alexander.kumar@duke.edu>
2024-05-02 02:05:35 -0400

    ignore ig

--- models/BaselineHash.py
+++ models/BaselineHash.py
@@ -20,6 +20,7 @@ class BaselineHash:
         '''
         self.data = data
         self.hash = hash
+        self.min_val, self.max_val = float("inf"), float("-inf")
         self.sort('digest')
 
     def generate_digests(self, col: str):
@@ -27,7 +28,11 @@ class BaselineHash:
         digest_series = pd.Series([''] * len(series))
 
         for i in range(len(series)):
-            digest_series[i] = self.hash(series[i])
+            digest = self.hash(series[i])
+            norm_digest = digest % self.data.__len__()
+            digest_series[i] = norm_digest
+            self.min_val = min(self.min_val, norm_digest)
+            self.max_val = max(self.max_val, norm_digest)
         
         self.data.add_series(col, digest_series)
 
@@ -38,7 +43,10 @@ class BaselineHash:
     def forward(self, keys: tuple):
         digests = [0] * len(keys)
         for i, key in enumerate(keys):
-            digests[i] = self.hash(key.encode())
+            digest = self.hash(key.encode())
+            norm_digest = digest % self.data.__len__()
+            # norm_digest = (digest - self.min_val) / self.max_val
+            digests[i] = norm_digest
         
         return Tensor(digests).unsqueeze(1)
     
